a57a69532bc041de4b86f1e498a21970568d30ae
solomon <solomon.doar@gmail.com>
2023-07-30 11:52:05 +0300

    Added running animation support in code

--- Assets/Scripts/Movement/MovementByVelocityEvent.cs
+++ Assets/Scripts/Movement/MovementByVelocityEvent.cs
@@ -8,9 +8,10 @@ public class MovementByVelocityEvent : MonoBehaviour
 {
     public event Action<MovementByVelocityEvent, MovementByVelocityArgs> onMovementByVelocity;
 
-    public void CallMovementByVelocityEvent(Vector2 moveDirection, float moveSpeed)
+    public void CallMovementByVelocityEvent(Vector2 moveDirection, float moveSpeed, bool isRunning)
     {
-        onMovementByVelocity?.Invoke(this, new MovementByVelocityArgs() { moveDirection = moveDirection, moveSpeed = moveSpeed });
+        onMovementByVelocity?.Invoke(this, new MovementByVelocityArgs() { moveDirection = moveDirection, moveSpeed = moveSpeed
+        , isRunning = isRunning});
     }
 
 }
@@ -19,4 +20,5 @@ public class MovementByVelocityArgs: EventArgs
 {
     public Vector2 moveDirection;
     public float moveSpeed;
+    public bool isRunning;
 }
